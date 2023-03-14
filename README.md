4eb58afde3fa876dc6c6e43a118b97391e3c6c94
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2023-03-14 11:18:08 -0400

    add dummy app and new_server.py

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 This will be the starting point for the Flask API used to capture images on the Oak-D CM4 Camera
+
+Setup instructions:
+
+1. install miniconda from here https://docs.conda.io/en/latest/miniconda.html
+2. on windows, open Anaconda Prompt
+3. cd `path/to/repo`
+4. conda create --name plantmap
+5. conda env create -f environment.yml
+6. pip install -r requirements.txt
---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+  return 'Server Works!'
+  
+@app.route('/greet')
+def say_hello():
+  return 'Hello from Server'
+
+@app.route('/hello')
+def hello():
+  return 'Hello, greetings from different endpoint'
+
+#adding variables
+@app.route('/user/<username>')
+def show_user(username):
+  #returns the username
+  return 'Username: %s' % username
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+  #returns the post, the post_id should be an int
+  return str(post_id)
