1581d07ed0a6e1506cf2b07bc4d44bbd4b941762
Mikah Pinegar <55995241+mikahpinegar@users.noreply.github.com>
2023-05-10 13:10:50 -0400

    add body

--- resources/status.py
+++ resources/status.py
@@ -1,11 +1,20 @@
-from flask_restful import Resource
+from flask_restful import reqparse, Resource
 
 # from common.device_maker import CameraDevice
 from common.image_collector import Collector
+
+parser = reqparse.RequestParser()
+parser.add_argument('TypeOfCrop')
+parser.add_argument('TypeOfCashCrop')
+parser.add_argument('TypeOfCoverCrops')
+
 STATUS = 'inactive'
 
 class Status(Resource):
-    def get(self, action):
+    def post(self, action):
+        args = parser.parse_args()
+        print(args)
+
         if action == 'start':
             STATUS = 'active'
 
@@ -15,4 +24,4 @@ class Status(Resource):
         else:
             return {'status': 'error', 'info': 'invalid option specified. use start or stop!'}, 400
         
-        return {'status': STATUS, 'time_elapsed': 0}, 200
+        return {'status': STATUS, 'args': args, 'time_elapsed': 0}, 200
